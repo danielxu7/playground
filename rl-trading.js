422b9e1109cd6f69eac1693f41891e1ce5a74796
danielxu7 <32692296+danielxu7@users.noreply.github.com>
2020-02-04 14:07:30 -0500

    added functionality to start automation after some time

--- rl-trading.js
+++ rl-trading.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rocket League Garage
 // @namespace    https://github.com/danielxu7/playground
-// @version      0.1
+// @version      1.0
 // @description  Automates RL trades.
 // @author       You
 // @match        https://rocket-league.com/trades/*
@@ -39,21 +39,19 @@
         myWindow.addEventListener('load', myLoad, false);
     }
     var initiate = function() {
+        $('#rlg-automate-trades').val('Updating trades!')
         updateTrade(0);        
     };
 
     // add a button to start automation
     $('body').append('<input type="button" value="Automate trades" id="rlg-automate-trades">')
     $('#rlg-automate-trades').css('position', 'fixed').css('top', 0).css('left', 0).css('z-index', 99999).css('color', 'black');
-    $('#rlg-automate-trades').click(function() {
-        $('#rlg-automate-trades').val('Updating trades!')
-        initiate();
-        setInterval(() => {
-            initiate();
-        }, 1000 * 60 * 60);
-    });
+    $('#rlg-automate-trades').click(initiate);
 
     $('.rlg-trade-display-container .rlg-trade-actions p a:nth-child(2)').each(function(index, element) {
         arr.push(element.href);
     });
+    setInterval(() => {
+        $('#rlg-automate-trades').click();
+    }, 1000 * 60 * 40);
 })();
